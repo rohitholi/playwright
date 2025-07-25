fe3fead39239edc4a532e302d653830bc6b69bda
Rohit <rohitholijkd@gmail.com>
2025-07-26 00:09:02 +0530

    pushing playwright2

--- src/test/java/AsimplePlaywrightTest.java
+++ src/test/java/AsimplePlaywrightTest.java
@@ -2,21 +2,41 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-public class AsimplePlaywrightTest
-{
-    @Test
-    void shouldShowPageTitle()
-    {
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-        Page page = browser.newPage();
+public class AsimplePlaywrightTest {
+    private static Playwright playwright;
+    private static Page page;
+    private static Browser browser;
+
+    @BeforeEach
+    void setup() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        page = browser.newPage();
         page.navigate("https://practicesoftwaretesting.com");
+        playwright.selectors().setTestIdAttribute("data-test");
+    }
+
+    @Test
+    void shouldShowPageTitle() {
         String title = page.title();
         System.out.println("Page title is: " + title);
-        playwright.close();
         Assertions.assertEquals("Practice Software Testing - Toolshop - v5.0", title, "Page title match expected value.");
     }
+
+    @Test
+    void search() {
+        page.locator("[placeholder='Search']").fill("Pliers");
+        page.getByTestId("search-submit").click();
+        int numberOfCards = page.locator(".card").count();
+        System.out.println("Number of cards found: " + numberOfCards);
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+     playwright.close();
+    }
+
 }
